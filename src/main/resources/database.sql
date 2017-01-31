e1bd3891b859413d5eaca7ac02618bb4e0eb3da2
Ihor0k <b.ihor@yandex.ru>
2017-02-01 01:42:21 +0200

    Fields length inrease

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -7,20 +7,20 @@ CREATE TABLE users (
 
 #Table: pages
 CREATE TABLE pages (
-  id          INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title       VARCHAR(255) NOT NULL,
-  url         VARCHAR(255) NOT NULL,
-  page_name   VARCHAR(255) NOT NULL,
-  description VARCHAR(1023)
+  id          INT           NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title       VARCHAR(4095) NOT NULL,
+  url         VARCHAR(4095) NOT NULL,
+  page_name   VARCHAR(4095) NOT NULL,
+  description TEXT
 );
 
 #Table: games
 CREATE TABLE games (
-  id         INT           NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  start_page INT           NOT NULL,
-  end_page   INT           NOT NULL,
-  pages_list VARCHAR(2047) NOT NULL,
-  user_id    INT           NOT NULL,
+  id         INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  start_page INT  NOT NULL,
+  end_page   INT  NOT NULL,
+  pages_list TEXT NOT NULL,
+  user_id    INT  NOT NULL,
 
   FOREIGN KEY (user_id) REFERENCES users (id),
   FOREIGN KEY (start_page) REFERENCES pages (id),
